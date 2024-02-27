d88593d35025f99bc6219425602ed3dca154faee
Enver Toleuov <anvarmtg@gmail.com>
2024-02-27 11:55:17 +0500

    toast message

--- build/1.Code.js
+++ build/1.Code.js
@@ -4,7 +4,8 @@ const active_sheet_name = "Active_sheet";
 const active_range = "B2:E";
 const category_sheet_name = "category_sheet";
 const category_range = "A2:D";
-const mlc = new MultiLevelDataValidation(active_sheet_name, active_range, category_sheet_name, category_range);
+const show_toast_message = true;
+const mlc = new MultiLevelDataValidation(active_sheet_name, active_range, category_sheet_name, category_range, show_toast_message);
 function onEdit(e) {
     mlc.onEdit(e);
 }
--- src/1.Code.ts
+++ src/1.Code.ts
@@ -3,9 +3,10 @@ const active_sheet_name = "Active_sheet";
 const active_range = "B2:E";
 const category_sheet_name = "category_sheet";
 const category_range = "A2:D";
+const show_toast_message = true;
 
-const mlc = new MultiLevelDataValidation(active_sheet_name, active_range, category_sheet_name, category_range)
+const mlc = new MultiLevelDataValidation(active_sheet_name, active_range, category_sheet_name, category_range, show_toast_message)
 
-function onEdit(e:GoogleAppsScript.Events.SheetsOnEdit) {
+function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
   mlc.onEdit(e)
 }
